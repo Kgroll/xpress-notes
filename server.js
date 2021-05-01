39a95cbeb5ee016715ff2500bcd4c803cb087bd0
Kristen Groller <groller@rogers.com>
2021-05-01 12:57:25 -0400

    finally workinggit add -A

--- server.js
+++ server.js
@@ -1,22 +1,93 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
-const PORT = process.env.PORT || 3001;
 const app = express();
 
-const apiRoutes = require('./routes/apiRoutes.js');
-const htmlRoutes = require('./routes/htmlRoutes.js');
+const PORT = process.env.PORT || 3004;
 
-app.use(express.urlencoded({ extended: true }));
+let notesData = [];
+
 app.use(express.json());
-app.use(express.static('public'));
-
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "./public")));
 
 
+// api routes
 
+app.get("/api/notes", function(err, res) {
+  try {
+    notesData = fs.readFileSync("./db/notes.json", "utf8");
+    notesData = JSON.parse(notesData);
+  } catch (err) {
+    console.log("\n error (in app.get.catch):");
+    console.log(err);
+  }
+  //   send objects to the browser
+  res.json(notesData);
+});
 
+// writes the new note to the json file
+app.post("/api/notes", function(req, res) {
+  try {
+    notesData = fs.readFileSync("./db/notes.json", "utf8");
+    console.log(notesData);
 
-app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
+    // parse the data to get an array of objects
+    notesData = JSON.parse(notesData);
+    req.body.id = notesData.length;
+    notesData.push(req.body); 
+    notesData = JSON.stringify(notesData);
+    // writes the new note to file
+    fs.writeFile("./db/notes.json", notesData, "utf8", function(err) {
+      if (err) throw err;
+    });
+    // changeit back to an array of objects & send it back to the browser(client)
+    res.json(JSON.parse(notesData));
+  } catch (err) {
+    throw err;
+    console.error(err);
+  }
+});
+
+// Delete a note
+
+app.delete("/api/notes/:id", function(req, res) {
+  try {
+    notesData = fs.readFileSync("./db/notes.json", "utf8");
+    notesData = JSON.parse(notesData);
+    notesData = notesData.filter(function(note) {
+      return note.id != req.params.id;
+    });
+
+    notesData = JSON.stringify(notesData);
+    fs.writeFile("./db/notes.json", notesData, "utf8", function(err) {
+      if (err) throw err;
+    });
+
+    // change it back to an array of objects & send it back to the browser (client)
+    res.send(JSON.parse(notesData));
+  } catch (err) {
+    throw err;
+    console.log(err);
+  }
+});
+
+// HTMLroutes
+
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+  return res.sendFile(path.json(__dirname, "./db/notes.json"));
+});
+
+// Start the server on the port
+app.listen(PORT, function() {
+    console.log(`API server now on port ${PORT}!`);
 });
